ce996339b97f85965d428b2f21398b15abbf27f2
sxv2721 <sxv2721@rit.edu>
2018-03-23 13:36:06 -0400

    ran through domoMaker C

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PART || 3000;
 
@@ -19,6 +23,18 @@ mongoose.connect(dbURL, (err) => {
     }
 });
 
+let redisURL = {
+    hostname: 'localhost',
+    port:6379,
+};
+
+let redisPASS;
+
+if (process.enc.REDISCLOUD_URL){
+    redisURL = url.parse(process.env.REDISCLOUD_URL);
+    redisPASS = redisURL.auth.split(';')[1];
+}
+
 const router = require("./router.js");
 
 const app = express();
@@ -30,14 +46,30 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(session({
     key: 'sessionid',
+    store: new RedisStore({
+        host: redisURL.hostname,
+        port: redisURL.port,
+        pass: redisPASS,
+    }),
     secret: 'Domo Arigato',
     resave: true,
     saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+    },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
+app.disable('x-powered-by');
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+    if(err.code !== 'EBADCSRFTOKEN') return next(err);
+    console.log('Missing CSRF token');
+    return false;
+});
 
 router(app);
 
